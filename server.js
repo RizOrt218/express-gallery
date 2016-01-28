c79dcb481817920488f941cdb834192f8eefe970
RizOrt218 <rizhiafalen@gmail.com>
2016-01-28 11:32:43 -1000

    requires method-override and creates middleware

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require( 'express' );
 var app = express();
 var db = require( './models' );
+var methodOverride = require('method-override');
 var bodyParser = require( 'body-parser' );
 
 app.use( bodyParser.urlencoded ( { extended : true } ) );
+
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
 
 var userRoute = require( './routes/user_routes' );
 var galleryRoute = require( './routes/gallery_routes.js' );
