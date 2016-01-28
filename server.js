4e9c0bf3195829646088e7ac5722627267889e19
Chaz <cmtlum73@gmail.com>
2016-01-27 19:16:08 -1000

    adds routes for gallery and adds jade view engine

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ var app = express();
 var db = require( './models' );
 var bodyParser = require( 'body-parser' );
 
-// var Gallery = db.Gallery;
 app.use( bodyParser.urlencoded ( { extended : true } ) );
 
 var userRoute = require( './routes/user_routes' );
-// var galleryRoute = require( './routes/gallery_routes.js' );
+var galleryRoute = require( './routes/gallery_routes.js' );
 
 app.use( '/users', userRoute );
-// app.use( '/gallery', galleryRoute );
+app.use( '/gallery', galleryRoute );
+
+app.set('view engine', 'jade');
+app.set('views', './templates');
 
 app.listen( 7766, function () {
   db.sequelize.sync();
