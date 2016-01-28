de12cef75c6ffcbe7e4260280f215dae23450002
RizOrt218 <rizhiafalen@gmail.com>
2016-01-28 10:32:08 -1000

    sequelizes path to handle CRUD population

--- routes/gallery_routes.js
+++ routes/gallery_routes.js
@@ -9,50 +9,69 @@ var bodyParser = require( 'body-parser' );
 
 router.use( bodyParser.urlencoded ( { extended : true } ) );
 
-
-router.get( '/', function ( req, res ) {
-  Gallery.findAll()
-    .then( function ( gallery ) {
-      res.render( 'landing_page.jade' );
-    });
-});
-
-
-router.get( '/:id', function ( req, res ) {
-
-});
-
-
-router.get( '/new', function ( req, res ) {
-
-});
-
-
-router.post( '/', function (req, res ) {
-  Gallery.create(
-    {
-      author: req.body.author,
-      link: req.body.link,
-      description: req.body.description
-    }
-  )
+router.route('/')
+  .get( function ( req, res ) {
+    Gallery.findAll()
+      .then( function ( gallery ) {
+        res.render( 'landing_page.jade' );
+      });
+  })
+  .post( function (req, res ) {
+    Gallery.create(
+      {
+        author: req.body.author,
+        link: req.body.link,
+        description: req.body.description
+      }
+    )
     .then( function ( gallery ) {
       res.json( gallery );
     });
 });
 
+router.route('/:id')
+  .get( function ( req, res ) {
+    Gallery.findAll({
+      where: {
+        id : req.params.id
+      }
+    })
+    .then( function ( gallery ) {
+      res.json( gallery );
+    });
+  })
+  .put( function ( req, res ) {
+    Gallery.update({
+      author: req.body.author,
+      link: req.body.link,
+      description: req.body.description
+    }, {
+      where : {
+        id : req.params.id
+      }
+    })
+    .then( function ( gallery ) {
+      res.json( gallery );
+    });
+  })
+  .delete( function ( req, res ) {
+    Gallery.destroy({
+      where : {
+        id : req.params.id
+      }
+    })
+    .then( function ( gallery ) {
+      res.json( gallery );
+    });
+  });
 
-router.get( '/:id/edit', function ( req, res ) {
+router.route('/new')
+  .get( function ( req, res ) {
 
 });
 
-
-router.put( '/:id', function ( req, res ) {
-
-});
-
-
-router.delete( '/:id', function ( req, res ) {
+router.route('/:id/edit')
+  .get( function ( req, res ) {
 
 });
 
